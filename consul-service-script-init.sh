e8cf590fe84feab0d45e72f4999d4237c28a5814
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 12:31:55 +0200

    7th commit

---
+++ consul-service-script-init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+cat > /etc/consul/consul-config.json << EOF
+{
+  "data_dir": "/opt/consul/data",
+  "client_addr": "0.0.0.0",
+  "retry_join": ["192.168.0.11"]
+}
+EOF
+
+cat > /etc/consul/consul-service.json << EOF
+{
+"service": {
+  "name": "web",
+  "port": 8080,
+  "check": {
+        "http": "http://localhost:8080",
+        "interval": "5s"
+    }
+  }
+}
+EOF
+
+-pid-file $PID_FILE 
+
+consul_pid=$(pgrep consul)
+
+kill $consul_pid
+echo "Kill status: $?"
+
+ip=$(ifconfig eth0 | grep 'inet ' | awk '{ print substr($2,1) }')
+consul agent -pid-file $PID_FILE -advertise $ip -config-file /etc/consul &

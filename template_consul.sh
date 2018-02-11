afa3216d4f9742264de98da4cd9d5ec195af721c
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 23:33:43 +0200

    consul template 2

--- template_consul.sh
+++ template_consul.sh
@@ -3,5 +3,5 @@ CONSUL_TEMPLATE_VERSION=0.15.0
 wget https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_386.zip -O consul-template_${CONSUL_TEMPLATE_VERSION}.zip
 
 unzip consul-template_${CONSUL_TEMPLATE_VERSION}.zip
-sudo chmod + consul-template_${CONSUL_TEMPLATE_VERSION}
-sudo mv consul-template_${CONSUL_TEMPLATE_VERSION} /usr/bin/consul-template_${CONSUL_TEMPLATE_VERSION}
+sudo chmod + consul-template
+sudo mv consul-template /usr/bin/consul-template

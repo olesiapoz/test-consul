0ac7531a37ca62f9840e5571d569be767adb37f0
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 17:41:00 +0200

    10th commit

--- docker_ngnix.sh
+++ docker_ngnix.sh
@@ -5,7 +5,7 @@ mkdir -p ~/consul/
 ip=$(ifconfig eth0 | grep 'inet ' | awk '{ print substr($2,1) }')
 echo "<h1>$ip $(hostname) <h1>" > ~/consul/ip.html
 
-docker container ls | grep "web" | awk '{ print $14 }' | xargs docker rm -fv
+docker container ls | grep "web" | awk '{ print $1 }' | xargs docker rm -fv
 
 
 #ngnix+docker+ mount volume(ip.htmp page)

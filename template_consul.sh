806d84686e1625adb80adf40d1549c77834a7588
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 23:32:09 +0200

    consul template 1

--- template_consul.sh
+++ template_consul.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CONSUL_TEMPLATE_VERSION=1.0.3
+CONSUL_TEMPLATE_VERSION=0.15.0
 wget https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_386.zip -O consul-template_${CONSUL_TEMPLATE_VERSION}.zip
 
 unzip consul-template_${CONSUL_TEMPLATE_VERSION}.zip

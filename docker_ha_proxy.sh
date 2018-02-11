245d4d73752dd2b592c29f7b150c276d527bd4e1
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 20:26:16 +0200

    ha-roxy commit 8

--- docker_ha_proxy.sh
+++ docker_ha_proxy.sh
@@ -6,7 +6,7 @@ docker container ls | grep "ha-proxy-lb" | awk '{ print $1 }' | xargs docker rm 
 #ha-proxy+docker+ mount volume(ip.htmp page)
 docker run -d \
 	--name ha-proxy-lb \
-	-p 8443:80 \
+	-p 80:8443 \
 	--restart unless-stopped \
 	-v /home/centos/test-consul-config-master/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
 	haproxy:1.6.5-alpine

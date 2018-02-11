2a61424c95880755327e9c7a337ac07401a386ac
olesiapoz <Olesiapoz@gmail.com>
2018-02-11 17:25:08 +0200

    9th commit

--- docker_ngnix.sh
+++ docker_ngnix.sh
@@ -4,9 +4,9 @@ mkdir -p ~/consul/
 # Fetch IP of teh node
 ip=$(ifconfig eth0 | grep 'inet ' | awk '{ print substr($2,1) }')
 echo "<h1>$ip $(hostname) <h1>" > ~/consul/ip.html
-docker rm web -f
-docker rm web
-docker volume prune
+
+docker container ls | grep "web" | awk '{ print $14 }' | xargs docker rm -fv
+
 
 #ngnix+docker+ mount volume(ip.htmp page)
 docker run -d \
